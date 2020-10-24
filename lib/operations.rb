4c5ea12c34f1a8a75184a973c5c0e7f5266b025e
Mnkith <munkithabid@gmail.com>
2020-10-24 02:32:55 -0500

    commit tree 278ec09770e38242cfd57e778a1a8318f58325bb

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,20 @@
 def unsafe?(speed)
-
+   return true if speed < 40 || speed > 60
+   false
 end
 
 
 
 def not_safe?(speed)
-	
+   # speed >= 40 && speed <= 60 ? false : true
+   speed >= 40 ? speed <= 60 ? false : true : true
 end
-	
+
+# test work around ;)
+# def not_safe?(speed)
+# 	return false if speed == 50
+#   return true
+#   puts " ? "
+# end
 
 
